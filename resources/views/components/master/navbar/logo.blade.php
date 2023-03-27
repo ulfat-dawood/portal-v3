d22a1f09ad4593cbdf784257ecd2de7d2f908aa9
ulfat-dawood <ulfat_dawood@outlook.com>
2023-03-27 14:58:34 +0300

    navbar logo image updated

--- resources/views/components/master/navbar/logo.blade.php
+++ resources/views/components/master/navbar/logo.blade.php
@@ -2,7 +2,7 @@
     <!-- LOGO  -->
     <div class=" h-11 w-40 ">
         <a href="{{ route('home', ['locale' => session('locale')]) }}">
-            <img src="{{ asset('assets/images/athir-logo-colorful.svg') }}" class="h-full" alt="">
+            <img src="{{ asset('assets/images/athircare_logo.png') }}" class="h-full" alt="">
         </a>
     </div>
 </div>
