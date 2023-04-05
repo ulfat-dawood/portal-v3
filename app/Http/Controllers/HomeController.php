345e5c89d117b24892b115de2200374af3a48590
Abdullahkimrigh <abdullahkimregh@gmail.com>
2023-04-05 13:41:36 +0300

    packages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\View\Components\packages;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return  view('index', [
-            'cities' => ['Jeddah', 'Makkah', 'Taif'],
-            'clinics' => ['Dental', 'General', 'Dermatology', 'Gynacology'],
-            'packages' => [
-                ['img'=> 'https://cdn.pixabay.com/photo/2014/12/10/21/01/doctor-563429_960_720.jpg', 'title'=> 'عرض الليزر', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
-                ['img'=> 'https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg', 'title'=> 'عرض العناية بالوجه', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
-                ['img'=> 'https://cdn.pixabay.com/photo/2016/09/14/20/50/tooth-1670434_960_720.png', 'title'=> 'عرض الأسنان', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
-                ['img'=> 'https://cdn.pixabay.com/photo/2014/08/26/21/54/dentist-428646_960_720.jpg', 'title'=> 'عرض تقويم الأسنان', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
-            ]
-        ]);
+    public function index()
+    {
+
+        $packages = Http::get(env('API_URL') . '/packages');
+        dd($packages);
+        // $packages = json_decode($apiResponse);
+        // dd($response);
+        // $packages = $response['data'];
+
+        // if (is_null($response)){
+        //     $packages = __('No Packages Available');
+        // } else {
+        //     $packages = $response;
+        // }
+
+        return view('index', compact('packages'));
+
+        // return  view('index', [
+        //     'cities' => ['Jeddah', 'Makkah', 'Taif'],
+        //     'clinics' => ['Dental', 'General', 'Dermatology', 'Gynacology'],
+        //     'packages' => [
+        //         ['img'=> 'https://cdn.pixabay.com/photo/2014/12/10/21/01/doctor-563429_960_720.jpg', 'title'=> 'عرض الليزر', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
+        //         ['img'=> 'https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg', 'title'=> 'عرض العناية بالوجه', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
+        //         ['img'=> 'https://cdn.pixabay.com/photo/2016/09/14/20/50/tooth-1670434_960_720.png', 'title'=> 'عرض الأسنان', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
+        //         ['img'=> 'https://cdn.pixabay.com/photo/2014/08/26/21/54/dentist-428646_960_720.jpg', 'title'=> 'عرض تقويم الأسنان', 'description'=> 'هنا وصف العرض', 'price'=> '500'],
+        //     ]
+        // ]);
     }
 }
--- resources/views/components/home/packages/index.blade.php
+++ resources/views/components/home/packages/index.blade.php
@@ -14,29 +14,31 @@
         <div class="packages-swiper-container relative">
             <div class="swiper doctors w-11/12 m-auto">
                 <div class="swiper-wrapper">
-                    @forelse ($packages as $package)
-                        <div class="swiper-slide mb-2">
-                            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                                <div class="bg-main-100 h-32 rounded-lg overflow-hidden relative">
-                                    <figure class="absolute left-0 right-0 top-0 bottom-0 w-full">
-                                        <img class="bg-main-50 object-cover w-full h-full" src="{{ $package['img'] }}"
-                                            alt="">
-                                    </figure>
-                                </div>
-                                <a href="#" class="px-8 py-4">
-                                    <div class="font-bold text-xl mb-2 mr-2">{{ $package['title'] }}</div>
-                                    <p class="text-gray-700 text-base mr-3"> {{ $package['description'] }} </p>
-                                </a>
-                                <div class="px-6 pt-4 pb-2">
-                                    <span
-                                        class="inline-block bg-orange-300 rounded-full px-3 py-1 text-sm font-semibold text-orange-600 mr-2 mb-2">
-                                        {{ $package['price'] }} @lang('SR')</span>
+                    @if (isset($packages))
+                        @foreach ($packages as $package)
+                            <div class="swiper-slide mb-2">
+                                <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                                    <div class="bg-main-100 h-32 rounded-lg overflow-hidden relative">
+                                        <figure class="absolute left-0 right-0 top-0 bottom-0 w-full">
+                                            <img class="bg-main-50 object-cover w-full h-full"
+                                                src="{{ $package['img'] }}" alt="">
+                                        </figure>
+                                    </div>
+                                    <a href="#" class="px-8 py-4">
+                                        <div class="font-bold text-xl mb-2 mr-2">{{ $package['title'] }}</div>
+                                        <p class="text-gray-700 text-base mr-3"> {{ $package['description'] }} </p>
+                                    </a>
+                                    <div class="px-6 pt-4 pb-2">
+                                        <span
+                                            class="inline-block bg-orange-300 rounded-full px-3 py-1 text-sm font-semibold text-orange-600 mr-2 mb-2">
+                                            {{ $package['price'] }} @lang('SR')</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @empty
-                        <div><span>@lang('No Packages Available')</span></div>
-                    @endforelse
+                        @endforeach
+                    @else
+                        <div><span class="text-center">@lang('No Packages Available')</span></div>
+                    @endif
                 </div>
             </div>
             <!-- navigation buttons -->
