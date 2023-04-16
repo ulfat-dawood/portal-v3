bed1f3814f3bb725cf78e02348f9bd4a448e3632
Abdullahkimrigh <abdullahkimregh@gmail.com>
2023-04-16 16:42:46 +0300

    enhancement packages style

--- resources/views/components/home/packages/index.blade.php
+++ resources/views/components/home/packages/index.blade.php
@@ -81,11 +81,11 @@
                         @foreach ($packages as $key => $package)
                             <a class="hover:bg-gray-100" href="{{ route('getPackages', ['locale' => session('locale'), 'packageId' => $key]) }}">
 
-                                <div class="flex-grow p-1">
-                                    <div class="flex felx-row rounded overflow-hidden shadow-lg mx-1 mb-1 mt-1 py-2 relative bg-white">
+                                <div class="flex-grow p-1 relative">
+                                    <div class="flex felx-row rounded overflow-hidden shadow-lg mx-1 mb-1 mt-1 py-2 static bg-white">
 
                                         <div>
-                                            <h1 class="flex text-md font-bold px-3 my-2">{{ $package['PKG_NAME'] }}</h1>
+                                            <h1 class="flex text-md font-bold px-3 w-7/12 my-2">{{ $package['PKG_NAME'] }}</h1>
                                             <h6 class="flex px-3 mx-2 p-3 text-gray-600 text-sm">{{ $package['PKG_DESC'] }}</h6>
                                             <i class="icofont-location-pin text-xs px-4 mx-2 text-gray-400"> {{ $package['HOSPITAL_NAME'] }}</i>
                                             <i class="icofont-location-pin text-xs px-4 mx-2 text-gray-400"> {{ $package['CITY_NAME'] }}</i>
@@ -93,7 +93,7 @@
 
                                         <div class="flex-shrink-0 absolute left-0 top-0 w-24 h-11 m-1 bg-main-500 rounded-xl text-center items-center">
                                             @if ($package['PKG_PRICE'] != $package['SRVC_PRICE'])
-                                                <h2 class="text-sm text-gray line-through">{{ $package['PKG_PRICE'] }} @lang('SR')</h2>
+                                                <h2 class="text-xs text-gray line-through">{{ $package['PKG_PRICE'] }} @lang('SR')</h2>
                                                 <h2 class="text-sm text-white">{{ $package['SRVC_PRICE'] }} @lang('SR')</h2>
                                             @else
                                                 <h2 class="text-sm text-white">{{ $package['PKG_PRICE'] }}</h2>
