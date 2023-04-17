998946f97b10b2b3af573d2f02115add5d51abcc
a6e6s <a6e6s1@gmail.com>
2023-04-17 10:55:28 +0300

    working on payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,13 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Paytabscom\Laravel_paytabs\Facades\paypage;
 
 class PaymentController extends Controller
 {
-    public function checkout(){
+    /**
+     * check out payment page for displaying credit card payment information
+     * @param array appointment data from session
+     */
+    public function checkout()
+    {
+        $data = (session('checkout'));
 
-        $data = session('checkout');
-        session()->forget('checkout');
-        dd($data);
+        $pay = paypage::sendPaymentCode('creditcard,mada,stcpay,applepay')
+            ->sendTransaction('sale')
+            ->sendCart(10, $data['slot']['EXAM_PRICE'], 'Appointment number :' . $data['slot']['CLIN_APPT_SLOT_ID'])
+            ->sendCustomerDetails($data['firstName'], $data['mobile'] . '@athir.com.sa', $data['mobile'], 'temp address', 'Jeddah', 'Makkah', 'SA', '12345', request()->ip())
+            ->sendShippingDetails($data['firstName'], $data['mobile'] . '@athir.com.sa', $data['mobile'], 'temp address', 'Jeddah', 'Makkah', 'SA', '12345', request()->ip())
+            // ->sendHideShipping(true)
+            ->sendFramed(true)
+            ->sendURLs(route('payment.response', ['locale' => app()->getLocale()]), route('payment.callback', ['locale' => app()->getLocale()]))
+            ->sendLanguage(app()->getLocale())
+            ->create_pay_page();
+
+        // return $pay;
+        return view('payment.checkout', ['url' => $pay]);
+    }
+    public function response(Request $request) //return
+    {
+        return '<script>window.parent.location.href = "'.route('payment.failed').'";</script>';
+        dd($request->query('respStatus'));
+    }
+    public function callback(Request $request) //callback
+    {
+        dd($request->query());
+    }
+    public function success(Request $request) //callback
+    {
+        dd($request->query());
+    }
+    public function failed(Request $request) //callback
+    {
+        dd($request->query());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,29 +20,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '{locale?}', 'middleware' => ['Localization']], function () {
 
-    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('', [HomeController::class, 'index'])->name('home');
 
     // Account
-    Route::get('/login', [AccountController::class, 'getRegistrationView'])->middleware('RedirectIfLoggedIn');
-    Route::post('/login', [AccountController::class, 'login'])->name('login');
-    Route::get('/register', [AccountController::class, 'getRegistrationView'])->middleware('RedirectIfLoggedIn');
-    Route::post('/register/otp', [AccountController::class, 'registrationOtp'])->name('register-otp');
-    Route::post('/register', [AccountController::class, 'register'])->name('register');
-    // Route::view('/registrationOtp', 'registration-otp')->name('registratio-otp-page');
-    // Route::post('/registrationOtp', [AccountController::class, 'verifyRegistrationOtp'])->name('registration-otp');
-    Route::get('/logout', [AccountController::class, 'logout'])->name('logout');
-    Route::get('/profile', [AccountController::class, 'getProfile'])->name('profile');
+    Route::get('login', [AccountController::class, 'getRegistrationView'])->middleware('RedirectIfLoggedIn');
+    Route::post('login', [AccountController::class, 'login'])->name('login');
+    Route::get('register', [AccountController::class, 'getRegistrationView'])->middleware('RedirectIfLoggedIn');
+    Route::post('register/otp', [AccountController::class, 'registrationOtp'])->name('register-otp');
+    Route::post('register', [AccountController::class, 'register'])->name('register');
+    // Route::view('registrationOtp', 'registration-otp')->name('registratio-otp-page');
+    // Route::post('registrationOtp', [AccountController::class, 'verifyRegistrationOtp'])->name('registration-otp');
+    Route::get('logout', [AccountController::class, 'logout'])->name('logout');
+    Route::get('profile', [AccountController::class, 'getProfile'])->name('profile');
 
     //Doctor
-    Route::get('/doctors', [DoctorController::class, 'getDoctors'] )->name('getDoctors');
-    Route::get('/doctor/{doctorName?}/{doctorId}/{centerId}/{clinicId}', [DoctorController::class, 'getDoctor'])->name('doctor');
+    Route::get('doctors', [DoctorController::class, 'getDoctors'])->name('getDoctors');
+    Route::get('doctor/{doctorName?}/{doctorId}/{centerId}/{clinicId}', [DoctorController::class, 'getDoctor'])->name('doctor');
 
     //Slots
-    Route::get('/slot/{slotId}', [SlotController::class, 'getSlot'])->name('slot');
+    Route::get('slot/{slotId}', [SlotController::class, 'getSlot'])->name('slot');
 
     // payment
-    Route::get('/checkout', [PaymentController::class, 'checkout'])->name('checkout');
-
-
-
+    Route::get('checkout', [PaymentController::class, 'checkout'])->name('checkout');
+    Route::get('payment/response', [PaymentController::class, 'response'])->name('payment.response');
+    Route::post('payment/callback', [PaymentController::class, 'callback'])->name('payment.callback');
+    Route::get('payment/success', [PaymentController::class, 'success'])->name('payment.success');
+    Route::get('payment/failed', [PaymentController::class, 'failed'])->name('payment.failed');
 });
