97f592b177836c39de8b7a2ac6268a9fe415fc85
a6e6s <a6e6s1@gmail.com>
2023-05-10 17:54:17 +0300

    debuging payment log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         // Use https in production
-        if (config('app.env') != 'local') {
+        if (config('app.env') == 'local') {
             \URL::forceScheme('https');
         }
     }
