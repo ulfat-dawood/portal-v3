efb543b1607da05f0e6bbac3c958a0d5c5a4c666
a6e6s <a6e6s1@gmail.com>
2023-05-10 10:25:13 +0300

    debuging payment log

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::any('test', function () {
-    dd(session('user'));
+    echo env('APP_ENV');
+    echo "/n/t <br/> ";
+    echo env('APP_DEBUG');
 });
 
 Route::group(['prefix' => '{locale?}', 'middleware' => ['Localization']], function () {
