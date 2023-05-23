c87e6b842b0ae5593b8a4bcd4cdaa331dc47b516
ulfat-dawood <ulfat_dawood@outlook.com>
2023-05-23 14:31:27 +0300

    APL-938 All three search boxes style error fixed

--- resources/views/livewire/home/search/home-visit-appt.blade.php
+++ resources/views/livewire/home/search/home-visit-appt.blade.php
@@ -1,83 +1,87 @@
-<div class="h-[272px]">
+<div>
+    <div class="h-[272px]">
 
-    <div class="error-messages pt-5">
+        <div class="error-messages pt-5">
 
-        @error('cityId')
-            <div class="text-sm text-secondary-400 text-center">@lang('Please select a city')</div>
-        @enderror
-        @error('clinicId')
-            <div class="text-sm text-secondary-400 text-center">@lang('Please select a speciality')</div>
-        @enderror
+            @error('cityId')
+                <div class="text-sm text-secondary-400 text-center">@lang('Please select a city')</div>
+            @enderror
+            @error('clinicId')
+                <div class="text-sm text-secondary-400 text-center">@lang('Please select a speciality')</div>
+            @enderror
+        </div>
+
+        <div class="flex flex-col pt-5 gap-5" wire:ignore>
+            {{-- CITY DROPDOWN   --}}
+            <select name="city" wire:model.lazy="cityId" data-placeholder="@lang('Select city')"
+                class="search-dropdown-home-vitit-appt rounded-b-lg flex-grow bg-grey-bg2 rounded-lg py-2 px-3">
+                {{-- placeholder --}}
+                <option value="">@lang('Select city')</option>
+                @foreach ($cities as $city)
+                    <option value="{{ $city['CITY_ID'] }}" data-display="{{ $city['CITY_NAME'] }}">
+                        {{ $city['CITY_NAME'] }}
+                    </option>
+                @endforeach
+            </select>
+
+            {{-- CLINIC DROPDOWN   --}}
+            <select name="clinic" wire:model.lazy="clinicId" data-placeholder="@lang('Select speciality')"
+                class="search-dropdown-home-vitit-appt rounded-b-lg flex-grow bg-grey-bg2 rounded-lg py-2 px-3">
+                {{-- placeholder --}}
+                <option value="">@lang('Select speciality')</option>
+                @foreach ($clinics as $clinic)
+                    <option value="{{ $clinic['CLINIC_ID'] }}" data-display="{{ $clinic['CLINIC_NAME'] }}">
+                        {{ $clinic['CLINIC_NAME'] }}
+                    </option>
+                @endforeach
+            </select>
+            <button wire:click="loadSearchResults"
+                class=" self-center bg-main-600 py-2 px-5 mt-3 rounded-lg text-grey-bg2 hover:bg-main-500">
+                @lang('Book a home visit')
+                <i class="icofont-search-1 text-grey-bg2 me-3"></i>
+            </button>
+        </div>
     </div>
 
-    <div class="flex flex-col pt-5 gap-5" wire:ignore>
-        {{-- CITY DROPDOWN   --}}
-        <select name="city" wire:model.lazy="cityId" data-placeholder="@lang('Select city')" class="search-dropdown-home-vitit-appt rounded-b-lg flex-grow bg-grey-bg2 rounded-lg py-2 px-3">
-            {{-- placeholder --}}
-            <option value="">@lang('Select city')</option>
-            @foreach ($cities as $city)
-                <option value="{{ $city['CITY_ID'] }}" data-display="{{ $city['CITY_NAME'] }}">
-                    {{ $city['CITY_NAME'] }}
-                </option>
-            @endforeach
-        </select>
-
-        {{-- CLINIC DROPDOWN   --}}
-        <select name="clinic" wire:model.lazy="clinicId" data-placeholder="@lang('Select speciality')" class="search-dropdown-home-vitit-appt rounded-b-lg flex-grow bg-grey-bg2 rounded-lg py-2 px-3">
-            {{-- placeholder --}}
-            <option value="">@lang('Select speciality')</option>
-            @foreach ($clinics as $clinic)
-                <option value="{{ $clinic['CLINIC_ID'] }}" data-display="{{ $clinic['CLINIC_NAME'] }}">
-                    {{ $clinic['CLINIC_NAME'] }}
-                </option>
-            @endforeach
-        </select>
-        <button wire:click="loadSearchResults" class=" self-center bg-main-600 py-2 px-5 mt-3 rounded-lg text-grey-bg2 hover:bg-main-500">
-            @lang('Book a home visit')
-            <i class="icofont-search-1 text-grey-bg2 me-3"></i>
-        </button>
-    </div>
-</div>
-
-<!-- Tom Select  -->
-<script src="https://cdn.jsdelivr.net/npm/tom-select@2.0.3/dist/js/tom-select.complete.min.js"></script>
-<script>
-    //Translate the terms of Tom Select:
-    var terms = {
-        add: 'Search for ',
-        noResult: 'No results'
-    };
-    var locale = document.getElementsByTagName("html")[0].getAttribute("lang");
-    if (locale == 'ar') {
-        terms.add = 'البحث عن';
-        terms.noResult = 'لا توجد نتائج';
-    }
-
-    //Clinics
-    let searchDropdownSetting = {
-        sortField: {
-            field: "text",
-            direction: "asc",
-        },
-        render: {
-
-            option: function(data, escape) {
-                return `<div class="flex justify-start gap-2 items-center">
-                        <span class="nxn-${escape(data.icon)}  text-main-600 w-3"></span>
-                        <span> ${escape(data.display)} </span>
-                    </div>`;
-            },
-            no_results: function(data, escape) {
-                return `<div class="p-1">${terms.noResult}</div>`;
-            },
-            //displayed inside the box as:
-            // item: function (data, escape) {
-            //     return `<div>  ${escape(data.display)}  </div>`;
-            // }
+    <!-- Tom Select  -->
+    <script src="https://cdn.jsdelivr.net/npm/tom-select@2.0.3/dist/js/tom-select.complete.min.js"></script>
+    <script>
+        //Translate the terms of Tom Select:
+        var terms = {
+            add: 'Search for ',
+            noResult: 'No results'
+        };
+        var locale = document.getElementsByTagName("html")[0].getAttribute("lang");
+        if (locale == 'ar') {
+            terms.add = 'البحث عن';
+            terms.noResult = 'لا توجد نتائج';
         }
-    }
-    document.querySelectorAll('.search-dropdown-home-vitit-appt').forEach(item => {
-        new TomSelect(item, searchDropdownSetting);
-    });
-</script>
+
+        //Clinics
+        let searchDropdownSetting = {
+            sortField: {
+                field: "text",
+                direction: "asc",
+            },
+            render: {
+
+                option: function(data, escape) {
+                    return `<div class="flex justify-start gap-2 items-center">
+                            <span class="nxn-${escape(data.icon)}  text-main-600 w-3"></span>
+                            <span> ${escape(data.display)} </span>
+                        </div>`;
+                },
+                no_results: function(data, escape) {
+                    return `<div class="p-1">${terms.noResult}</div>`;
+                },
+                //displayed inside the box as:
+                // item: function (data, escape) {
+                //     return `<div>  ${escape(data.display)}  </div>`;
+                // }
+            }
+        }
+        document.querySelectorAll('.search-dropdown-home-vitit-appt').forEach(item => {
+            new TomSelect(item, searchDropdownSetting);
+        });
+    </script>
 </div>
